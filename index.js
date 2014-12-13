3a181d3f3c6638920b6e01317b1f8195c89b7357
Sushant Gupta <sushant711@gmail.com>
2014-12-13 20:34:06 +0530

    added quick intersection

--- index.js
+++ index.js
@@ -2,147 +2,161 @@ var _ = require('underscore');
 
 // Replica of python range method.
 exports.range = function( n ) {
-    var arr = [];
-    for( var i = 0; i < (n || arr.length); ++i ) arr[i] = i;
-    return arr;
+  var arr = [];
+  for( var i = 0; i < (n || arr.length); ++i ) arr[i] = i;
+  return arr;
 };
 
 // From array return array of chunks
 exports.chunkify = function (arr, chunkSize) {
-    arr = arr || [];
-    chunkSize = chunkSize > 0 ? chunkSize : arr.length;
+  arr = arr || [];
+  chunkSize = chunkSize > 0 ? chunkSize : arr.length;
 
-    var result = [];
-    var buffer = [];
-    var j = 0;
-    for (var i = 0; i < arr.length; i++, j++) {
-        if (j == chunkSize) {
-            result.push(buffer);
-            j = 0;
-            buffer = [];
-        }
-        buffer.push(arr[i]);
+  var result = [];
+  var buffer = [];
+  var j = 0;
+  for (var i = 0; i < arr.length; i++, j++) {
+    if (j == chunkSize) {
+      result.push(buffer);
+      j = 0;
+      buffer = [];
     }
-    if (buffer.length > 0) {
-        result.push(buffer);
-    }
-    return result;
+    buffer.push(arr[i]);
+  }
+  if (buffer.length > 0) {
+    result.push(buffer);
+  }
+  return result;
 };
 
 /* Inverts javascript object.
-    - example :
-        - input :
-            - {a : 1, A : 1, b : 2, B : 2}
-        - output :
-            - {'1' : ['a', 'A'], '2' : ['b', 'B']}
+  - example :
+    - input :
+      - {a : 1, A : 1, b : 2, B : 2}
+    - output :
+      - {'1' : ['a', 'A'], '2' : ['b', 'B']}
 */
 exports.invertObj = function (obj) {
-    var result = {};
-    _.each(obj, function (value, key) {
-        if (_.has(result, value)) {
-            result[value].push(key);
-        } else {
-            result[value] = [key];
-        }
-    });
-    return result;
+  var result = {};
+  _.each(obj, function (value, key) {
+    if (_.has(result, value)) {
+      result[value].push(key);
+    } else {
+      result[value] = [key];
+    }
+  });
+  return result;
 };
 
 /* Unwinds an index
-    - example :
-        - input :
-            - {'1': ['a', 'A'], '2': ['b', 'B']}
-        - output :
-            - {'a': '1', 'A': '1', 'b': '2', 'B': '2'}
+  - example :
+    - input :
+      - {'1': ['a', 'A'], '2': ['b', 'B']}
+    - output :
+      - {'a': '1', 'A': '1', 'b': '2', 'B': '2'}
 */
 exports.unwindIndex = function (index) {
-    var result = {};
-    _.each(index, function(value, key) {
-        _.each(value, function(elem) {
-            result[elem] = key;
-        });
+  var result = {};
+  _.each(index, function(value, key) {
+    _.each(value, function(elem) {
+      result[elem] = key;
     });
+  });
 
-    return result;
+  return result;
 };
 
 /*
-    An extension to underscore findWhere method, works with nested properties.
-    constraints -> [{fields: ['k1', 'k2', 'k3'], value: 'val'}, ...]
-    returns index / indexes
+  An extension to underscore findWhere method, works with nested properties.
+  constraints -> [{fields: ['k1', 'k2', 'k3'], value: 'val'}, ...]
+  returns index / indexes
 */
 exports.nestedFindWhere = function(itr, constraints, options) {
-    options = options || {};
-    result = []
-    for (var index = 0; index < itr.length; index++) {
-        var element = itr[index];
-        var isOk = true;
-        for (var i = 0; i < constraints.length; i++) {
-            var terminal = element;
-            for (var j = 0; j < constraints[i].fields.length; j++) {
-                if (_.isObject(terminal) && _.has(terminal, constraints[i].fields[j])) {
-                    terminal = terminal[constraints[i].fields[j]];
-                } else {
-                    isOk = false;
-                    break;
-                }
-            }
+  options = options || {};
+  result = []
+  for (var index = 0; index < itr.length; index++) {
+    var element = itr[index];
+    var isOk = true;
+    for (var i = 0; i < constraints.length; i++) {
+      var terminal = element;
+      for (var j = 0; j < constraints[i].fields.length; j++) {
+        if (_.isObject(terminal) && _.has(terminal, constraints[i].fields[j])) {
+          terminal = terminal[constraints[i].fields[j]];
+        } else {
+          isOk = false;
+          break;
+        }
+      }
 
-            if (!isOk || !_.isEqual(terminal, constraints[i].value)) {
-                isOk = false;
-                break;
-            }
-        }
-        if (isOk) {
-            if (options.onlyFirst) {
-                return index;
-            }
-            result.push(index);
-        }
+      if (!isOk || !_.isEqual(terminal, constraints[i].value)) {
+        isOk = false;
+        break;
+      }
     }
-    if (options.onlyFirst) return -1;
-    return result;
+    if (isOk) {
+      if (options.onlyFirst) {
+        return index;
+      }
+      result.push(index);
+    }
+  }
+  if (options.onlyFirst) return -1;
+  return result;
+};
+
+exports.quickIntersection = function(l1, l2) {
+  var result = [];
+  var l1_index = {};
+  _.each(l1, function(elem) {
+    l1_index[elem] = null;
+  });
+  _.each(l2, function(elem) {
+    if (_.has(l1_index, elem)) {
+      result.push(elem);
+    }
+  });
+  return result;
 };
 
 var listify = function(itr) {
-    var list = [];
-    if (_.isString(itr)) {
-        list.push(itr);
-    } else if (_.isArray(itr)) {
-        _.each(itr, function(val) {
-            list = [].concat(list, listify(val));
-        });
-    } else if (_.isObject(itr)) {
-        _.each(itr, function(val, key) {
-            list = [].concat(list, listify(key), listify(val));
-        });
-    }
-    return list;
+  var list = [];
+  if (_.isString(itr)) {
+    list.push(itr);
+  } else if (_.isArray(itr)) {
+    _.each(itr, function(val) {
+      list = [].concat(list, listify(val));
+    });
+  } else if (_.isObject(itr)) {
+    _.each(itr, function(val, key) {
+      list = [].concat(list, listify(key), listify(val));
+    });
+  }
+  return list;
 };
 
 exports.listify = listify;
 
 // Serialize parameters in object for GET request.
 exports.serialize = function(obj) {
-    var str = [];
-    for (var p in obj) {
-        str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
-    }
-    return str.join("&");
+  var str = [];
+  for (var p in obj) {
+    str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
+  }
+  return str.join("&");
 }
 
 // Escapes / sanitize string for dynamic regular expressions.
 exports.escapeRegExp = function (str) {
-  return str.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, "\\$&");
+ return str.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, "\\$&");
 };
 
 
 // Trims and truncates multiple spaces to single space.
 exports.cleanSpacing = function (str) {
-    return str.replace(/(?:(?:^|\n)\s+|\s+(?:$|\n))/g,'').replace(/\s+/g,' ');
+  return str.replace(/(?:(?:^|\n)\s+|\s+(?:$|\n))/g,'').replace(/\s+/g,' ');
 };
 
 exports.ellipsis = function (str, n) {
-    n = n > 4 ? n: 4;
-    return str.length > n ? str.substring(0, n - 3) + '...' : str;
+  n = n > 4 ? n: 4;
+  return str.length > n ? str.substring(0, n - 3) + '...' : str;
 };
